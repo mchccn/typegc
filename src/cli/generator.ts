b7ad77100b4555ec94db1efa26f80917f30e2707
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-15 15:03:41 -0500

    complete range factory js

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -435,7 +435,29 @@ class Resolver {
                             throw new RangeError(`Expected 1-3 arguments for the range factory, got none.`);
                         }, {
                             ts: `string | number`,
-                            js: `() => true`, // ! Add range factory JS (instead of checking parameters per function call, check parameters, then return different functions based on the parameters to make it easier to write the JS for the range factory)
+                            js: typeof start === "number" && typeof stop === "number" && typeof step === "number" ? `(v) => {
+    if (typeof v === "string") return new RangeError(\`String values cannot use the step parameter for the range factory.\`);
+
+    if (stop <= start) return new RangeError(\`Stop parameter must be greater than the start parameter in the range factory.\`);
+
+    if (step <= 0) return new RangeError(\`Step parameter for the range factory must be positive.\`);
+
+    return v >= start && v <= stop && ((v - start) % step) === 0;
+}` : typeof start === "number" && typeof stop === "number" ? `(v) => {
+    if (stop <= start) return new RangeError(\`Stop parameter must be greater than the start parameter in the range factory.\`);
+
+    if (typeof v === "string") return v.length >= start && v.length <= stop;
+
+    return v >= start && v <= stop;
+}` : typeof start === "number" ? `(v) => {
+    if (start < 0) {
+        if (typeof v === "string") throw new RangeError(\`String values cannot use a negative end parameter in the range factory.\`);
+
+        return v >= start;
+    }
+
+    return typeof v === "string" ? v.length <= start : v <= start && v >= 0;
+}` : `() => false`,
                             global: ``,
                         });
                     },
