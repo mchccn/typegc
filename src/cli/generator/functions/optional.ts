57db464b13150d545d53f8b06b668e286db3ae0f
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-19 15:56:50 -0500

    start on optional factory

---
+++ src/cli/generator/functions/optional.ts
@@ -0,0 +1,17 @@
+import type { Constraint } from "../types";
+import { uuid } from "../uuid";
+
+throw new Error(`fix this later`);
+
+export default (fn: Constraint) => {
+  if (typeof fn !== "function") return new TypeError(`Argument passed to the optional factory must be a constraint.`);
+
+  const id = uuid.next().value;
+
+  return (Object.assign((v: unknown) => fn(v) || typeof v === "undefined", {
+    ts: `${fn.ts} | undefined`,
+    js: `(v) => {{ name }}${id}()(v) || typeof v === "undefined"`,
+    global: ``, // ! like the generator, use cached getters
+    dependencies: [[`{{ name }}${id}`, fn]],
+  });
+}
