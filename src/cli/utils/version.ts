6ec1949965cbe71508b6d43d3620c1f102fbac01
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-19 10:47:49 -0500

    Update version.ts

--- src/cli/utils/version.ts
+++ src/cli/utils/version.ts
@@ -4,5 +4,5 @@ import { join } from "path";
 let cachedversion = "";
 
 export function version() {
-    return cachedverison || (cachedversion = JSON.parse(readFileSync(join(__dirname, "..", "..", "..", "package.json"), "utf8")).version);
+    return cachedversion || (cachedversion = JSON.parse(readFileSync(join(__dirname, "..", "..", "..", "package.json"), "utf8")).version);
 }
