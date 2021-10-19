1dd38ad6b8a4835bc02a18b205b4afcba3e80c6e
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-19 09:28:35 -0500

    create interpret func

--- src/interpret.ts
+++ src/interpret.ts
@@ -1,1 +1,9 @@
-export function interpret() {}
+import { Tokenizer } from "./cli/generator/tokenizer";
+import { Parser } from "./cli/generator/parser";
+import { Resolver } from "./cli/generator/resolver";
+
+export function interpret<Schema extends string>(schema: Schema) {
+  const resolved = new Resolver(new Parser(new Tokenizer(schema).tokenize()).parse()).resolve();
+  
+  return Object.fromEntries([...resolved.models.entries()].map(([name, model]) => [name, model.bind({})] as [string, (v: unknown) => boolean]);
+}
