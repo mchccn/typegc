27af3d04a789c1d385bbd7a4a1160e7c17aac336
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-19 13:38:52 -0500

    match factory input checks

--- src/cli/generator/functions/match.ts
+++ src/cli/generator/functions/match.ts
@@ -1,7 +1,11 @@
 import { uuid } from "../uuid";
 
 export default (pattern: string, flags?: string) => {
+    if (typeof pattern !== "string") return new TypeError(`Pattern provided to the match factory must be a string.`);
+    
     if (!pattern) return new TypeError(`Pattern provided to the match factory cannot be empty.`);
+    
+    if (typeof flags !== "undefined" && typeof flags !== "string") return new TypeError(`Flags provided to the match factory must be a string.`);
 
     const regex = new RegExp(pattern, flags);
 
