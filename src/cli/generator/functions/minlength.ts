9e429f4b6cc5e0a2fa514514e075442f8087f205
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-19 14:07:34 -0500

    Update minlength.ts

--- src/cli/generator/functions/minlength.ts
+++ src/cli/generator/functions/minlength.ts
@@ -1,5 +1,5 @@
 export default (min: number, exclusive?: boolean) => {
-  return Object.assign((v: number) => exclusive ? v.length > min : v.length >= min, {
+  return Object.assign((v: string) => exclusive ? v.length > min : v.length >= min, {
     ts: `number`,
     js: exclusive ? `(v) => v.length > ${min}` : `(v) => v.length >= ${min}`,
     global: ``,
