3228fa7376789eb473508c443ac035493637b718
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-19 09:47:09 -0500

    smol bin.ts

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -2,14 +2,12 @@
 
 import index from ".";
 
-(async () => {
+(() => {
   if (require.main === module) {
     process.env.TYPEGC_EXEC_TYPE = "cli";
 
-    await index();
-    
-    return;
+    return index();
   }
   
   throw new Error("Do not import the CLI when using TypeGC in your application.");
-})().catch(console.error);
+})();
