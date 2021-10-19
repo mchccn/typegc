a32c2dc645155102ddc207081348dbc5b3eccbf6
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-10-19 13:41:28 -0500

    update min factory to work

--- src/cli/generator/functions/min.ts
+++ src/cli/generator/functions/min.ts
@@ -1,7 +1,7 @@
 export default (min: number, exclusive?: boolean) => {
   return Object.assign((v: number) => exclusive ? v > min : v >= min, {
     ts: `number`,
-    js: exclusive ? `(v) => v > min` : `(v) => v >= min`,
+    js: exclusive ? `(v) => v > ${min}` : `(v) => v >= ${min}`,
     global: ``,
   });
 };
